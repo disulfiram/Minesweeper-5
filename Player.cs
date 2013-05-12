15850ed5b8ad0bbe6edf473287970f7fc44df758
disulfiram <plamen.aaylasecura@gmail.com>
2013-05-12 11:26:49 +0300

    XML comments added in Player class
    Class may need some more tinkering in the future. (CompareTo)

--- Player.cs
+++ Player.cs
@@ -1,41 +1,87 @@
 ﻿namespace Minesweeper
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
 
+    /// <summary>
+    /// Class for player.
+    /// </summary>
     public class Player : IComparable
     {
+        /// <summary>
+        /// Name of player.
+        /// </summary>
         private string name;
+
+        /// <summary>
+        /// Score of player.
+        /// </summary>
         private int score;
 
+        /// <summary>
+        /// Initializes a new instance of the Player class.
+        /// </summary>
+        /// <param name="name">Name of player.</param>
+        /// <param name="score">Score of player.</param>
         public Player(string name, int score)
         {
-            this.name = name;
-            this.score = score;
+            this.Name = name;
+            this.Score = score;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the player.
+        /// </summary>
         public string Name
         {
-            get { return this.name; }
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("The name of the player cannot be null.");
+                }
+                this.name = value;
+            }
         }
 
+        /// <summary>
+        /// Gets or sets the name of the player.
+        /// </summary>
         public int Score
         {
-            get { return this.score; }
+            get
+            {
+                return this.score;
+            }
+            set
+            {
+                this.score = value;
+            }
         }
 
+        /// <summary>
+        /// Compares two instances of the Player class by their scores.
+        /// </summary>
+        /// <param name="obj">The other Player.</param>
+        /// <returns>1 if the this player has higher score than the second one. -1 if this player has lower score than the second one</returns>
         public int CompareTo(object obj)
         {
             if (!(obj is Player))
             {
-                throw new ArgumentException(
-                   "A Player object is required for comparison.");
+                throw new ArgumentException("A Player object is required for comparison.");
             }
 
             return -1 * this.score.CompareTo(((Player)obj).score);
         }
 
+        /// <summary>
+        /// Creates a string of information about an instance of the class Player.
+        /// </summary>
+        /// <returns>String containing information about the instance of the class Player.</returns>
         public override string ToString()
         {
             string result = this.name + " --> " + this.score;
