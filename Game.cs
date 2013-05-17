6a292885f537a6d5e3a616d0e4c1dce539f902e8
disulfiram <plamen.aaylasecura@gmail.com>
2013-05-17 12:55:46 +0300

    High score extracted
    -Extracted high score logic in a separate class
    -Added xml comments in Game class

--- Game.cs
+++ Game.cs
@@ -1,79 +1,63 @@
 namespace Minesweeper
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
-
+    
     public class Game
     {
+        /// <summary>
+        /// Board number of rows.
+        /// </summary>
         private const int MaxRows = 5;
+        
+        /// <summary>
+        /// Board number of columns.
+        /// </summary>
         private const int MaxColumns = 10;
+
+        /// <summary>
+        /// Amount of mines on the board.
+        /// </summary>
         private const int MaxMines = 15;
+
+        /// <summary>
+        /// Maximum number of players in highscore board.
+        /// </summary>
         private const int MaxTopPlayers = 5;
 
+        /// <summary>
+        /// Highscore board.
+        /// </summary>
+        private static HighScore highScore;
+
+        /// <summary>
+        /// Game board.
+        /// </summary>
         private static Board board;
-        private static List<Player> topPlayers;
 
         public static void Main(string[] args)
         {
             Menu();
         }
 
+        /// <summary>
+        /// Initializes a game board.
+        /// </summary>
         private static void InitializeGameBoard()
         {
             board = new Board(MaxRows, MaxColumns, MaxMines);
         }
 
+        /// <summary>
+        /// Initializes highscore board.
+        /// </summary>
         private static void InitializeTopPlayers()
         {
-            topPlayers = new List<Player>();
-            topPlayers.Capacity = MaxTopPlayers;
+            highScore = new HighScore();
         }
 
-        private static bool IsQualifiedForScoreBoard(int score)
-        {
-            if (topPlayers.Count < topPlayers.Capacity)
-            {
-                return true;
-            }
-
-            foreach (Player currentPlayer in topPlayers)
-            {
-                if (currentPlayer.Score < score)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private static void AddPlayerToScoreBoard(Player player)
-        {
-            if (topPlayers.Capacity > topPlayers.Count)
-            {
-                topPlayers.Add(player);
-                topPlayers.Sort();
-            }
-            else
-            {
-                topPlayers.RemoveAt(topPlayers.Capacity - 1);
-                topPlayers.Add(player);
-                topPlayers.Sort();
-            }
-        }
-
-        private static void ListTopPlayers()
-        {
-            Console.WriteLine("Scoreboard");
-            for (int i = 0; i < topPlayers.Count; i++)
-            {
-                Console.WriteLine(i + 1 + ". " + topPlayers[i]);
-            }
-
-            Console.ReadKey();
-        }
-
+        /// <summary>
+        /// Main method for the game.
+        /// </summary>
         private static void Menu()
         {
             InitializeTopPlayers();
@@ -110,15 +94,22 @@ namespace Minesweeper
             Console.Read();
         }
 
+        /// <summary>
+        /// Prints the initial greating for the player.
+        /// </summary>
         private static void PrintHeader()
         {
             Console.Clear();
             Console.WriteLine("Welcome to the game “Minesweeper”. " +
-                                      "Try to reveal all cells without mines. " +
-                                      "Use 'top' to view the scoreboard, 'restart' to start a new game " +
-                                      "and 'exit' to quit the game.");
+                              "Try to reveal all cells without mines. " +
+                              "Use 'top' to view the scoreboard, 'restart' to start a new game " +
+                              "and 'exit' to quit the game.");
         }
 
+        /// <summary>
+        /// Processes input.
+        /// </summary>
+        /// <param name="input">Input from console.</param>
         private static void ProcessCommands(string input)
         {
             if (input == "restart")
@@ -127,14 +118,19 @@ namespace Minesweeper
             }
             else if (input == "top")
             {
-                ListTopPlayers();
+                highScore.ListTopPlayers();
             }
         }
 
+        /// <summary>
+        /// Performes operations concerning the turn of a player.
+        /// </summary>
+        /// <param name="row">Player chosen row.</param>
+        /// <param name="col">Player chosen column.</param>
         private static void PlayMove(int row, int col)
         {
-            if (row < 0 || row >= MaxRows
-                || col < 0 || col >= MaxColumns)
+            if (row < 0 || row >= MaxRows ||
+                col < 0 || col >= MaxColumns)
             {
                 Console.WriteLine("Illegal move.");
                 Console.ReadKey();
@@ -150,15 +146,15 @@ namespace Minesweeper
                 if (status == Board.Status.SteppedOnAMine)
                 {
                     Console.WriteLine(Environment.NewLine + "Booooom! You were killed by a mine. You revealed " +
-                                        score +
-                                        " cells without mines.");
+                                      score +
+                                      " cells without mines.");
                 }
                 else
                 {
                     Console.WriteLine("Congratulations! You win!!");
                 }
 
-                if (IsQualifiedForScoreBoard(score))
+                if (highScore.IsQualifiedForScoreBoard(score))
                 {
                     SaveHighScore(score);
                 }
@@ -172,13 +168,17 @@ namespace Minesweeper
             }
         }
 
+        /// <summary>
+        /// Performs operations on highscore board after game is over.
+        /// </summary>
+        /// <param name="score">Score of player.</param>
         private static void SaveHighScore(int score)
         {
             Console.WriteLine("Please enter your name for the top scoreboard: ");
             string name = Console.ReadLine();
             Player player = new Player(name, score);
-            AddPlayerToScoreBoard(player);
-            ListTopPlayers();
+            highScore.AddPlayerToScoreBoard(player);
+            highScore.ListTopPlayers();
         }
     }
 }
---
+++ HighScore.cs
@@ -0,0 +1,82 @@
+﻿namespace Minesweeper
+{
+    using System;
+    using System.Collections.Generic;
+    
+    class HighScore
+    {
+        /// <summary>
+        /// The number of highscores the list contains
+        /// </summary>
+        const int MaxPlayers = 5;
+
+        /// <summary>
+        /// List of players that stores the highscores
+        /// </summary>
+        private List<Player> topPlayers;
+
+        /// <summary>
+        /// Initializes an instance of the Highscore class.
+        /// </summary>
+        public HighScore()
+        {
+            this.topPlayers = new List<Player>(MaxPlayers);
+        }
+
+        /// <summary>
+        /// Checks if player score is high enough to be entered in the board.
+        /// </summary>
+        /// <param name="score">Score of player.</param>
+        /// <returns>True if player has high enough score. False if player does not have high enough score.</returns>
+        internal bool IsQualifiedForScoreBoard(int score)
+        {
+            if (topPlayers.Count < topPlayers.Capacity)
+            {
+                return true;
+            }
+
+            foreach (Player currentPlayer in topPlayers)
+            {
+                if (currentPlayer.Score < score)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Adds player to the scoreboard.
+        /// </summary>
+        /// <param name="player">Player that is being added to scoreboard.</param>
+        internal void AddPlayerToScoreBoard(Player player)
+        {
+            if (topPlayers.Capacity > topPlayers.Count)
+            {
+                topPlayers.Add(player);
+                topPlayers.Sort();
+            }
+            else
+            {
+                topPlayers.RemoveAt(topPlayers.Capacity - 1);
+                topPlayers.Add(player);
+                topPlayers.Sort();
+            }
+        }
+
+        /// <summary>
+        /// Prints highscores on the console.
+        /// </summary>
+        internal void ListTopPlayers()
+        {
+            Console.WriteLine("Scoreboard");
+            for (int i = 0; i < topPlayers.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + topPlayers[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
